8f081c4abdfc197f09e6b27486ac176cbbe9c138
Ashar Khan <asharullah.khan2468@gmail.com>
2024-10-09 22:00:41 +0500

    practice nextjs

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-center font-semibold mt-4'>About</div>
+  )
+}
+
+export default page
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 const Navbar:React.FC = () => {
 
   return (
-    <nav className="bg-background/50 sticky border-b backdrop-blur">
+    <nav className=" bg-transparent sticky border-1 top-0 backdrop-blur">
       <div className="container mx-auto px-4 ">
         <div className="flex justify-between items-center py-4">
           <div className="text-black text-2xl font-bold">Ashar</div>
           <div className="hidden md:flex space-x-8">
-            <a href="#home" className="text-black-300 hover:text-white">Home</a>
-            <a href="#about" className="text-black-300 hover:text-white">About</a>
-            <a href="#projects" className="text-black-300 hover:text-white">Projects</a>
-            <a href="#contact" className="text-black-300 hover:text-white">Contact</a>
+            <Link href="/" className="text-black font-semibold  hover:text-black">Home</Link>
+            <Link href="/about" className="text-black font-semibold hover:text-black">About</Link>
+            <Link href="/projects" className="text-black font-semibold hover:text-black">Projects</Link>
+            <Link href="/contact" className="text-black font-semibold hover:text-black">Contact</Link>
           </div>
           <div className="md:hidden flex items-center">
+          <Sheet>
+          <SheetTrigger>
             <button
-              className="text-gray-300 hover:text-white focus:outline-none"
+              className="text-black hover:text-black focus:outline-none"
               id="menu-toggle"
             >
               <svg
@@ -29,19 +41,46 @@ const Navbar:React.FC = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
             </button>
+            </SheetTrigger>
+
+            <SheetContent>
+    <SheetHeader>
+      <SheetTitle className='text-2xl font-bold'>Ashar</SheetTitle>
+      <SheetDescription>
+      <div id="menu" className='mt-4' >
+          <a href="#home" className="block px-4 py-2 text-black-300 hover:bg-gray-700 hover:text-white">Home</a>
+          <a href="#about" className="block px-4 py-2 text-black-300 hover:bg-gray-700 hover:text-white">About</a>
+          <a href="#projects" className="block px-4 py-2 text-black-300 hover:bg-gray-700 hover:text-white">Projects</a>
+          <a href="#contact" className="block px-4 py-2 text-black-300 hover:bg-gray-700 hover:text-white">Contact</a>
+        </div>
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+  </Sheet>
+            
+    
           </div>
         </div>
       </div>
 
       {/* Mobile Menu */}
       <div className="md:hidden">
-        <div id="menu" className="hidden">
-          <a href="#home" className="block px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
-          <a href="#about" className="block px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-          <a href="#projects" className="block px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-          <a href="#contact" className="block px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-        </div>
+
+      
+
+  
+
+
+        
       </div>
+
+      
+ 
+ 
+
+
+
+
     </nav>
   );
 };
